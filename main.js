729b9ae72bff2b401e4d61a0376cba6253ccbfa3
kit jonathan ho <j.hokit80@gmail.com>
2016-09-22 01:21:31 +0800

    basic functions completed

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
 $(document).ready(function () {
 
 //check off a todo by clicking
-$('li').click(function(){
+$('ul').on ("click", "li", function(){
   $(this).toggleClass("completed");
-  //ALTERNATIVE
-  // if ($(this).css("color") === "rgb(128, 128, 128)") {
-  //   $(this).css({
-  //     color: "black",
-  //     textDecoration: "none"
-  //   })
-  // } else {
-  //   $(this).css({
-  //     color: "grey",
-  //     textDecoration: "line-through"
-  //   });
-  // }
 });
 
 
+//click on X to delete todo
+$('ul').on ("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  //stop effect to parents
+  event.stopPropagation();
+})
 
-
+$("input[type='text']").keypress( function(event){
+  if(event.which === 13) {
+    //grab todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span>X </span>" + todoText + "</li>")
+  }
+})
 
 
 
