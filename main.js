1164bd42fc4f008e987c96c90c5e7b11ba36a1fb
kit jonathan ho <j.hokit80@gmail.com>
2016-09-22 00:38:11 +0800

    checkoff function

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 $(document).ready(function () {
 
+//check off a todo by clicking
+$('li').click(function(){
+  $(this).toggleClass("completed");
+  //ALTERNATIVE
+  // if ($(this).css("color") === "rgb(128, 128, 128)") {
+  //   $(this).css({
+  //     color: "black",
+  //     textDecoration: "none"
+  //   })
+  // } else {
+  //   $(this).css({
+  //     color: "grey",
+  //     textDecoration: "line-through"
+  //   });
+  // }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
